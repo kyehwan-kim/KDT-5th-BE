0f62189b2b4216acd467624683ff6fbf326ea01f
kyehwan <clon0306@gmail.com>
2023-03-07 15:46:36 +0900

    BE: callback

---
+++ knock.js
@@ -0,0 +1,14 @@
+function knockDoor(time, name, callback) {
+  console.log(`노크를 하고 기다립니다.`);
+
+  setTimeout(() => {
+    // 실행하고픈 코드
+    callback(time, name);
+  }, 1000 * time);
+}
+
+function callName(time, name) {
+  console.log(`${name}이가 ${time}초 만에 문을 열고 나왔습니다.`);
+}
+
+knockDoor(3, '영식', callName);
